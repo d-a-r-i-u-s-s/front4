738d29ce5c88cfb4c158215a99f0c86ba8eec016
d-a-r-i-u-s-s <72103576+d-a-r-i-u-s-s@users.noreply.github.com>
2021-01-14 02:21:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -30,39 +30,42 @@ window.onload = function(){
     request('GET', 'https://jsonplaceholder.typicode.com/posts/', function(response) { // Call a function when the state changes.
         if (response.target.readyState === XMLHttpRequest.DONE && response.target.status === 200) {
             // Request finished. Do processing here.
-            posts = response.target;
-            JSON.parse(response.target.response).slice(0,5).forEach(post => {
+            posts =  JSON.parse(response.target.response);
+            
+            posts.slice(0,5).forEach(post => {
                 let temp_post = template(post.id, post.title, post.body)
                 let table = document.getElementById("TableContent");
                 table.append(temp_post)
             });
         }
-        $("button").click(function (e) {
-            e.preventDefault();
-            var but_id = this.value;
-            $("#PutForm").css("display", "flex");
-            console.log(posts);
-            $("#PutForm input[name='title']").val(posts[but_id - 1].title);
-            $("#PutForm input[name='body']").val(posts[but_id - 1].body);
-            $("#PutForm").submit(function (e) {
-            e.preventDefault();
-            let put_data = {
-            title: $("#PutForm input[name='title']").val(),
-            body: $("#PutForm input[name='body']").val()
-            };
-    
-            $.ajax({
-            url: 'https://jsonplaceholder.typicode.com/posts/'+ but_id,
-            type: 'PUT',
-            data: JSON.stringify(put_data),
-            success: function(data_2) {
-            console.log(data_2);
-            }
-            });
-    
-            $("#PutForm").css("display", "none");
-        });
-        });
+            $("td button").click(function (e) {
+                e.preventDefault();
+                let f = 0;
+                var but_id = this.value;
+                $("#PutForm").css("display", "flex");
+                $("#PutForm input[name='title']").val(posts[but_id - 1].title);
+                $("#PutForm input[name='body']").val(posts[but_id - 1].body);
+                $("#PutForm button").click( function(e) {
+                e.preventDefault();
+                let put_data = {
+                title: $("#PutForm input[name='title']").val(),
+                body: $("#PutForm input[name='body']").val()
+                };
+                if (f == 0) {
+                    $.ajax({
+                        url: 'https://jsonplaceholder.typicode.com/posts/'+ but_id,
+                        type: 'PUT',
+                        data: JSON.stringify(put_data),
+                        success: function(data_2) {
+                        console.log(data_2);
+                        
+                        }
+                        });
+                    };
+                f = 1;  
+                $("#PutForm").css("display", "none");
+                });
+            }); 
     })
 }
 const request = function(method, url, callback){
